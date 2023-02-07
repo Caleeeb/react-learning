5ccb72a7b1d816f706290249411b9eab68ba8679
Caleb Day <day.caleb@gmail.com>
2023-02-07 11:01:06 -0500

    add description

--- src/App.js
+++ src/App.js
@@ -17,13 +17,28 @@ function App() {
         <section className="section">
           <div className="columns">
             <div className="column is-4">
-              <ProfileCard title='Alexa' handle='@alexa99' image={CortanaImage} />
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={CortanaImage}
+                description="This is a digital assistant that helps you buy stuff on Amazon."
+              />
             </div>
             <div className="column is-4">
-              <ProfileCard title='Cortana' handle='@cortana44' image={AlexaImage} />
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana44"
+                image={AlexaImage}
+                description="This is a digital assistant that Microsoft made to battle Siri."
+              />
             </div>
             <div className="column is-4">
-              <ProfileCard title='Siri' handle='@siri101' image={SiriImage} />
+              <ProfileCard
+                title="Siri"
+                handle="@siri101"
+                image={SiriImage}
+                description="This is a ddigital assistant that has not been updated in a long time."
+              />
             </div>
           </div>
         </section>
